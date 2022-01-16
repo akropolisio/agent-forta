5b2fced3b9e729a7fb43537ede99cb277a715399
joshualyguessennd <joshualyguessennd@icloud.com>
2022-01-16 02:57:26 +0000

    feat: agent

--- src/agent.py
+++ src/agent.py
@@ -1,2 +1,38 @@
 from forta_agent import Finding, FindingType, FindingSeverity
+from .constants import (
+    MY_ADDRESS
+)
 
+def handle_transaction(transaction_event):
+    findings = []
+    # filter the transaction from the contract
+    multisig_invocations = transaction_event.filter_function(
+        # multi-sig_pausable_abi, and multi-sig-address
+    )
+
+    # filter transaction info from multi-sig
+    for invocation in multisig_invocations:
+        sender = transaction_event
+
+        findings.append(
+            Finding(
+                {
+                    "name": "MultiSig function listening",
+                    "description": f"Function call by sender",
+                    "type": Finding.suspicious,
+                    "severity": get_severity(sender),
+                    "metadata": {
+                        "from": transaction_event.from_,
+                        "to": transaction_event.to_
+                    }
+                }
+            )
+        )
+    return findings
+
+
+def get_severity(sender):
+    if sender == MY_ADDRESS:
+        return FindingSeverity.Medium
+    else:
+        return FindingSeverity.Critical
---
+++ src/send_txs.py
@@ -0,0 +1,8 @@
+from forta_agent import get_web3_provider
+from web3 import Web3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#####
