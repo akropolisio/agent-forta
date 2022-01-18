deebceab4deb897b800dd224d7d7d57f8858f592
joshualyguessennd <joshualyguessennd@icloud.com>
2022-01-18 02:25:30 +0000

    feat: event ApprovedHash

--- src/constants.py
+++ src/constants.py
@@ -1,41 +1,21 @@
 MULTI_SIG_MAINNET_ADDRESS = "0xAE28420C1adC4a3bba95c1e97a2a85b8E1149114"
-# MULTI_SIG_ABI = """
-# {
-#     "anonymous":false,
-#     "inputs":[
-#         {
-#             "indexed":true,
-#             "internalType":"bytes32",
-#             "name":"approvedHash",
-#             "type":"bytes32"
-#         },
-#         {
-#             "indexed":true,
-#             "internalType":"address",
-#             "name":"owner",
-#             "type":"address"
-#         }
-#     ],
-#     "name":"ApproveHash",
-#     "type":"event"
-# }"""
 MULTI_SIG_ABI = """
 {
     "anonymous":false,
     "inputs":[
         {
-            "indexed":false,
+            "indexed":true,
             "internalType":"bytes32",
-            "name":"txHash",
+            "name":"approvedHash",
             "type":"bytes32"
         },
         {
-            "indexed":false,
-            "internalType":"uint256",
-            "name":"payment",
-            "type":"uint256"
+            "indexed":true,
+            "internalType":"address",
+            "name":"owner",
+            "type":"address"
         }
     ],
-    "name":"ExecutionSuccess",
+    "name":"ApproveHash",
     "type":"event"
 }"""
