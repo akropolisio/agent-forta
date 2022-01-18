365b53241fc8c95cc92316867cddeb912349a131
joshualyguessennd <joshualyguessennd@icloud.com>
2022-01-18 02:25:10 +0000

    feat: event ApprovedHash

--- src/agent_test.py
+++ src/agent_test.py
@@ -1,4 +1,5 @@
 import json
+from pydoc_data.topics import topics
 import eth_abi
 from eth_utils import event_abi_to_log_topic
 from forta_agent import create_transaction_event
@@ -10,17 +11,9 @@ class TestMultiSigEvent:
     
     def test_returns_empty_findings_if_address_is_wrong(self):
         data = eth_abi.encode_abi(["address"], [MULTI_SIG_MAINNET_ADDRESS])
-        topics = [event_abi_to_log_topic(json.loads(MULTI_SIG_ABI)), filter]
+        topics = [event_abi_to_log_topic(json.loads(MULTI_SIG_ABI)), data]
         tx_event = create_transaction_event({
-            'receipt': {
-                'logs': [
-                    {
-                        'topics': topics,
-                        'data': data,
-                        'address': "0x1010101010101010101010101010101010101010"
-                    }
-                ]
-            },
+            #
         })
         findings = handle_transaction(tx_event)
         assert len(findings) == 0
--- src/constants.py
+++ src/constants.py
@@ -1,21 +1,41 @@
-MULTI_SIG_MAINNET_ADDRESS = "0xC5aF91F7D10dDe118992ecf536Ed227f276EC60D"
+MULTI_SIG_MAINNET_ADDRESS = "0xAE28420C1adC4a3bba95c1e97a2a85b8E1149114"
+# MULTI_SIG_ABI = """
+# {
+#     "anonymous":false,
+#     "inputs":[
+#         {
+#             "indexed":true,
+#             "internalType":"bytes32",
+#             "name":"approvedHash",
+#             "type":"bytes32"
+#         },
+#         {
+#             "indexed":true,
+#             "internalType":"address",
+#             "name":"owner",
+#             "type":"address"
+#         }
+#     ],
+#     "name":"ApproveHash",
+#     "type":"event"
+# }"""
 MULTI_SIG_ABI = """
 {
     "anonymous":false,
     "inputs":[
         {
-            "indexed":true,
+            "indexed":false,
             "internalType":"bytes32",
-            "name":"approvedHash",
+            "name":"txHash",
             "type":"bytes32"
         },
         {
-            "indexed":true,
-            "internalType":"address",
-            "name":"owner",
-            "type":"address"
+            "indexed":false,
+            "internalType":"uint256",
+            "name":"payment",
+            "type":"uint256"
         }
     ],
-    "name":"ApproveHash",
+    "name":"ExecutionSuccess",
     "type":"event"
 }"""
