7c19448bff31b7978d8be57c08f04b7eba2e4e62
joshualyguessennd <joshualyguessennd@icloud.com>
2022-02-01 16:45:47 +0400

    feat: fix test

--- src/agent.py
+++ src/agent.py
@@ -1,23 +1,47 @@
-import forta_agent
 from forta_agent import Finding, FindingType, FindingSeverity
 from src.constants import MULTI_SIG_MAINNET_ADDRESS, ADDRESS_LIST
 
-def handle_transaction(transaction_event: forta_agent.transaction_event.TransactionEvent):
+# def handle_transaction(transaction_event):
+#     findings = []
+#     if transaction_event.to != MULTI_SIG_MAINNET_ADDRESS:
+#         return findings
+#     targeted_address = transaction_event.from_
+#     if targeted_address in ADDRESS_LIST:
+#         findings.append(
+#             Finding({
+#                 'name': 'multisig tracker',
+#                 'description': f'track interaction between {targeted_address} with multisig protocol',
+#                 'type': FindingType.Info,
+#                 'alert_id': "MultiSig_ALERT",
+#                 'severity': FindingSeverity.Info,
+#                 'metadata': {
+#                     "address": targeted_address
+#                 }
+#             })
+#         )
+#     return findings
+def handle_transaction(transaction_event):
     findings = []
-    if transaction_event.to != MULTI_SIG_MAINNET_ADDRESS:
+
+    listenning_address = transaction_event.to
+    sender = transaction_event.from_
+
+    if listenning_address != MULTI_SIG_MAINNET_ADDRESS:
         return findings
-    targeted_address = transaction_event.from_
-    if targeted_address in ADDRESS_LIST:
-        findings.append(
-            Finding({
-                'name': 'multisig tracker',
-                'description': f'track interaction between {targeted_address} with multisig protocol',
-                'type': FindingType.Info,
-                'alert_id': "MultiSig_ALERT",
-                'severity': FindingSeverity.Info,
-                'metadata': {
-                    "address": targeted_address
-                }
-            })
-        )
+    
+
+    findings.append(
+        Finding({
+            "name": "Akropolis Wallet Tracker",
+            "description": "track wallet interaction",
+            "alert_id": "AKRO-1",
+            "type": FindingType.Info,
+            "severity": FindingSeverity.Info,
+            "metadata": {
+                "sender": sender
+            }
+        }
+    ))
+    
     return findings
+    
--- src/agent_test.py
+++ src/agent_test.py
@@ -1,4 +1,4 @@
-from forta_agent import FindingSeverity, FindingType, create_transaction_event
+from forta_agent import create_transaction_event
 from agent import handle_transaction
 from src.constants import MULTI_SIG_MAINNET_ADDRESS, ADDRESS_LIST
 
@@ -10,17 +10,19 @@ class TestMultiSigInteraction:
                 'to': MULTI_SIG_MAINNET_ADDRESS,
             }})
         findings = handle_transaction(tx_event)
-        assert len(findings) == 0
+        assert len(findings) != 0
 
     def test_returns_finding_if_found(self):
         tx_event = create_transaction_event({
             'transaction': {
-                'from': ADDRESS_LIST[0],
+                'from': ADDRESS_LIST[2],
                 'to': MULTI_SIG_MAINNET_ADDRESS
             }
         })
         findings = handle_transaction(tx_event)
+        print(tx_event.status)
         assert len(findings) != 0
+        
 
     def test_returns_empty_if_not_multisig(self):
         tx_event = create_transaction_event({
